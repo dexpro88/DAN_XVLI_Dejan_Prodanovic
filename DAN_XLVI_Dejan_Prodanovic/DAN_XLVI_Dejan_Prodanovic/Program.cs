34b19b7b4865a36ad7250ccd96612659145fd468
dexpro988 <dejan.prodanovic@panonit.com>
2020-07-16 05:29:47 -0700

    Add sempaphore

--- DAN_XLVI_Dejan_Prodanovic/DAN_XLVI_Dejan_Prodanovic/Program.cs
+++ DAN_XLVI_Dejan_Prodanovic/DAN_XLVI_Dejan_Prodanovic/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DAN_XLVI_Dejan_Prodanovic
@@ -11,9 +12,21 @@ namespace DAN_XLVI_Dejan_Prodanovic
         static void Main(string[] args)
         {
             Vehicles veh = new Vehicles();
-            veh.GenerateVehicles();
+            veh.GenerateVehicles(PrintVehiclesInfo);
             Console.WriteLine("All vehicles crossed the bridge");
             Console.ReadLine();
+        }
+
+        public static void PrintVehiclesInfo(Dictionary<Thread, string> vehicles)
+        {
+            Console.WriteLine("Total number of vehicles {0}", vehicles.Count);
+            Console.WriteLine("\nList of vehicles and their directions");
+            Console.WriteLine("----------------------------------------");
+            foreach (KeyValuePair<Thread, string> vehicle in vehicles)
+            {
+                Console.WriteLine("{0} {1}",
+               vehicle.Key.Name, vehicle.Value);
+            }
         }
     }
 }
--- DAN_XLVI_Dejan_Prodanovic/DAN_XLVI_Dejan_Prodanovic/Vehicles.cs
+++ DAN_XLVI_Dejan_Prodanovic/DAN_XLVI_Dejan_Prodanovic/Vehicles.cs
@@ -9,38 +9,51 @@ namespace DAN_XLVI_Dejan_Prodanovic
 {
     class Vehicles
     {
-        public delegate void PrintInfo(int numberOfVehicles, Dictionary<Thread, string> vehicles);
+        private SemaphoreSlim semaphore = new SemaphoreSlim(0, 3);
+        public delegate void PrintInfo(Dictionary<Thread, string> dictionary);
         //List<Thread> vehicles = new List<Thread>();
         Dictionary<Thread,string> vehicles = new Dictionary<Thread, string>();
         string[] directions = { "south","north"};
         Random rnd = new Random();
 
-        public void GenerateVehicles()
+        public void GenerateVehicles(PrintInfo printInfo)
         {
             int numberOfVehicles = rnd.Next(1,16);
-            int directionIndex = rnd.Next(0,2);
+           
 
             for (int i = 0; i < numberOfVehicles; i++)
             {
+                int directionIndex = rnd.Next(0, 2);
                 string direction = directions[directionIndex];
                 Thread thread = new Thread(() => CrossTheBridge(direction));
                 thread.Name = String.Format("Vehicle{0}", i + 1);
-                //vehicles.Add(thread);
-                vehicles.Add(thread,direction);
-                thread.Start();
+               
+                vehicles.Add(thread,direction);              
             }
+
+            printInfo(vehicles);
+
             foreach (KeyValuePair<Thread, string> veh in vehicles)
             {
-
+                veh.Key.Start();
+                
+            }
+            foreach (KeyValuePair<Thread, string> veh in vehicles)
+            {              
                 veh.Key.Join();
             }
         }
 
         public void CrossTheBridge(string direction)
         {
-            Thread.Sleep(3000);
-            string vehicleName = Thread.CurrentThread.Name;
-            Console.WriteLine("{0} crossed the bridge", vehicleName);
+            Console.WriteLine("{0} waits on the semaphore to cross the bridge ",Thread.CurrentThread.Name);
+               
+            semaphore.Wait();
+            //Thread.Sleep(3000);
+            //string vehicleName = Thread.CurrentThread.Name;
+            //Console.WriteLine("{0} crossed the bridge", vehicleName);
         }
+
+       
     }
 }
